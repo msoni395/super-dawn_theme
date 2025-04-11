df056fe68e9220b2a569aa92e274556559f3de54
Mitesh kumar <46155336+msoni395@users.noreply.github.com>
2025-04-11 19:48:13 +0530

    Add files via upload

---
+++ assets/popup.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function(){
+        document.getElementById('popup').style.display = 'block';
+    }, 5000); // Adjust this number to change the time delay
+});
